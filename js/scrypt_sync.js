116a13dedb67334a5cd27619ddcf9e9396708ec4
hassemer-g <g.hassemer@protonmail.com>
2025-09-13 21:43:09 -0400

    Update scrypt_sync.js

--- js/scrypt_sync.js
+++ js/scrypt_sync.js
@@ -3,10 +3,6 @@ import { doHashing } from "./deriv.js";
 import { encodeBase91 } from "./base91.js";
 
 
-
-
-
-
 function doScrypt(
     passw, 
     salt, 
@@ -14,23 +10,6 @@ function doScrypt(
     outputLength = 64, 
 ) {
 
-    
-    if (
-        arguments.length < 2
-        || arguments.length > 4
-        || !(passw instanceof Uint8Array)
-        || !(salt instanceof Uint8Array)
-        || !Number.isSafeInteger(cost)
-        || cost < 1
-        || cost > 20
-        || !Number.isSafeInteger(outputLength)
-        || outputLength < 1
-        || outputLength > 64
-    ) {
-        throw new Error(`Incorrect arguments passed to the "doScrypt" function.`);
-    }
-
-    
     const output = scrypt(
         passw,
         salt,
@@ -46,7 +25,6 @@ function doScrypt(
 }
 
 
-
 export function multScrypt(
     passw, 
     salt, 
@@ -56,33 +34,10 @@ export function multScrypt(
     encodingFunction = encodeBase91,
 ) {
 
-    
-    if (
-        arguments.length < 3
-        || arguments.length > 6
-        || !(passw instanceof Uint8Array)
-        || !(salt instanceof Uint8Array)
-        || !Number.isSafeInteger(iterations)
-        || iterations < 1
-        || !Number.isSafeInteger(cost)
-        || cost < 1
-        || cost > 20
-        || !Number.isSafeInteger(outputLength)
-        || outputLength < 1
-        || outputLength > 64
-        || typeof encodingFunction !== "function"
-    ) {
-        throw new Error(`Invalid inputs received by the "multScrypt" function!`);
-    }
-
-    
     let output = doHashing(`—${encodingFunction(passw)}—${iterations}—${cost}—${outputLength}—`);
 
-    
     let counter = 1;
     do {
-
-        console.log(`Scrypt rounds: ${counter}/${iterations}`);
 
         salt = doHashing(`—${counter}—${encodingFunction(salt)}—${iterations}—${cost}—${outputLength}—`);
 
@@ -97,17 +52,6 @@ export function multScrypt(
 
     } while (!(counter > iterations));
 
-    
-    if (
-        !(output instanceof Uint8Array)
-        || output.length !== outputLength
-        || !(salt instanceof Uint8Array)
-        || salt.length !== 64
-    ) {
-        throw new Error(`Function "multScrypt" failed.`);
-    }
-
     return output;
 }
-
 
