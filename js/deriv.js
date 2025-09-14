f7b8316c21cac3df433484a69770f181ff65a1f6
hassemer-g <g.hassemer@protonmail.com>
2025-09-13 21:33:21 -0400

    Update deriv.js

--- js/deriv.js
+++ js/deriv.js
@@ -1,14 +1,10 @@
-import { sha512 } from "./noble-hashes/sha2.js";
+import { sha256, sha512 } from "./noble-hashes/sha2.js";
 import { sha3_512 } from "./noble-hashes/sha3.js";
 import { blake2b, blake2s } from "./noble-hashes/blake2.js";
 import { blake3 } from "./noble-hashes/blake3.js";
 import { hkdf } from "./noble-hashes/hkdf.js";
 import { utf8ToBytes, concatBytes } from "./noble-hashes/utils.js";
 import { encodeBase91 } from "./base91.js";
-
-
-
-
 
 
 export function derivSingle(
@@ -20,7 +16,6 @@ export function derivSingle(
     algoForInfo = blake2b,
 ) {
 
-    
     if (
         arguments.length < 2
         || arguments.length > 6
@@ -36,7 +31,6 @@ export function derivSingle(
         throw new Error(`Incorrect arguments passed to the "derivSingle" function.`);
     }
 
-    
     const output = hkdf(
         algoForHKDF,
         passw,
@@ -45,17 +39,8 @@ export function derivSingle(
         outputLength,
     );
 
-    
-    if (
-        !(output instanceof Uint8Array)
-        || output.length !== outputLength
-    ) {
-        throw new Error(`Function "derivSingle" failed.`);
-    }
-
     return output; 
 }
-
 
 
 export function doHashing(
@@ -63,7 +48,6 @@ export function doHashing(
     info = "", 
     outputLength = 64,
 ) {
-
     
     if (
         arguments.length < 1
@@ -75,10 +59,9 @@ export function doHashing(
     ) {
         throw new Error(`Incorrect arguments passed to the "doHashing" function.`);
     }
-
     
     if (input instanceof Uint8Array) {
-        
+        // do nothing
     } else if (typeof input === "string" && input.trim()) {
         input = utf8ToBytes(input);
     } else if (Number.isSafeInteger(input) || typeof input === "bigint") {
@@ -89,42 +72,25 @@ export function doHashing(
         throw new Error(`Invalid input! Acceptable input types to the "doHashing" function: Uint8Array, non-empty string, integer, big integer, object or array.`);
     }
 
-    const hash1 = blake3(input); 
-    const hash2 = sha512(input); 
-    const hash3 = sha3_512(input); 
-    const hash4 = blake2b(input); 
-    const hash5 = blake2s(input); 
+    const hash1 = sha256(input);
+    const hash2 = sha512(input);
+    const hash3 = sha3_512(input);
+    const hash4 = blake2b(input);
+    const hash5 = blake2s(input);
+    const hash6 = blake3(input);
 
-    const passw = concatBytes(hash1, hash2, hash3, hash4, hash5); 
-    const preSalt = concatBytes(hash2, hash3, hash4, hash5, hash1); 
-
-
-    
-    if (
-        !(passw instanceof Uint8Array)
-        || passw.length !== 256
-        || !(preSalt instanceof Uint8Array)
-        || preSalt.length !== 256
-    ) {
-        throw new Error(`Derivation with "doHashing" failed.`);
-    }
-
+    const passw = concatBytes(hash1, hash2, hash3, hash4, hash5, hash6);
+    const salt = concatBytes(hash2, hash3, hash4, hash5, hash6, hash1);
 
     const output = derivSingle(
-        passw, 
-        sha3_512(preSalt), 
+        passw,
+        salt,
         `"doHashing" — ${outputLength} — ${info}`, 
         outputLength,
     );
 
-    
-    if (!(output instanceof Uint8Array) || output.length !== outputLength) {
-        throw new Error(`Function "doHashing" failed.`);
-    }
-
     return output; 
 }
-
 
 
 export function derivMult(
@@ -138,7 +104,6 @@ export function derivMult(
     encodingFunction = encodeBase91,
 ) {
 
-    
     if (
         arguments.length < 3
         || arguments.length > 8
@@ -173,18 +138,8 @@ export function derivMult(
         ));
     }
 
-    
-    if (
-        !Array.isArray(elements)
-        || elements.length !== numberOfElements
-        || !elements.every(v => v instanceof Uint8Array && v.length === outputLength)
-    ) {
-        throw new Error(`Function "derivMult" failed.`);
-    }
-
     return elements; 
 }
-
 
 
 export function expandKey(
@@ -196,10 +151,6 @@ export function expandKey(
     algoForInfo = blake2b,
     encodingFunction = encodeBase91,
 ) {
-
-    
-    console.time("Key expansion");
-
     
     if (
         arguments.length < 3
@@ -216,7 +167,6 @@ export function expandKey(
         throw new Error(`Incorrect arguments passed to the "expandKey" function.`);
     }
 
-    
     let expandedKey = doHashing(`—${expandedKeyLength}—${encodingFunction(passw)}—`);
 
     for (let i = 1; i < Math.ceil(expandedKeyLength / 64); i++) {
@@ -224,18 +174,6 @@ export function expandKey(
         salt = doHashing(`—${i}—${encodingFunction(salt)}—`);
 
         const tempPassw = doHashing(`—${i}—${encodingFunction(concatBytes(expandedKey.slice(-32), expandedKey.slice(0, 32)))}—`);
-
-
-        
-        if (
-            !(salt instanceof Uint8Array)
-            || salt.length !== 64
-            || !(tempPassw instanceof Uint8Array)
-            || tempPassw.length !== 64
-        ) {
-            throw new Error(`Key expansion failed.`);
-        }
-
 
         const newPiece = derivSingle(
             tempPassw,
@@ -248,24 +186,14 @@ export function expandKey(
 
         expandedKey = i % 2 === 0 ? concatBytes(newPiece, expandedKey) : concatBytes(expandedKey, newPiece);
     }
-
     
     for (let i = 1; expandedKey.length > expandedKeyLength; i++) {
         expandedKey = i % 2 === 0 ? expandedKey.slice(0, expandedKey.length - 1) : expandedKey.slice(1);
     }
-
-    
-
     
     if (!(expandedKey instanceof Uint8Array) || expandedKey.length !== expandedKeyLength) {
         throw new Error(`Function "expandKey" failed.`);
     }
 
-    console.timeEnd("Key expansion");
-
     return expandedKey; 
 }
-
-
-
-
